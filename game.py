c304ae2531714b7641452f18ef3985e44cc28ec1
Yesica N Mendoza <yesicanmendoza@gmail.com>
2022-02-01 03:59:51 +0000

    Thinking a number

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
 """A number-guessing game."""
+import random
 
 # Put your code here
 
 print("Howdy, what's your name?")
 name = input("(type in your name) ")
+print(name, "I'm thinking of a number between 1 and 100.")
+print("Try to guess my number.")
+my_number = random.randint(1,100)
