502bf3d49fd8a42a3a94cae0f26aaba25c87d222
Yesica N Mendoza <yesicanmendoza@gmail.com>
2022-02-01 04:15:49 +0000

    Game complete

--- game.py
+++ game.py
@@ -8,3 +8,19 @@ name = input("(type in your name) ")
 print(name, "I'm thinking of a number between 1 and 100.")
 print("Try to guess my number.")
 my_number = random.randint(1,100)
+
+counter = 0
+while True:
+    guess=int(input("Your guess? " ))
+    counter = counter + 1
+    if guess > my_number:
+        print("Your guess is too high, try again.")
+    elif guess < my_number:
+        print("Your guess is too low, try again.")
+    else:
+        break
+
+print("Well done,", name,"! You found my number in", counter ,"tries!")
+
+    
+
